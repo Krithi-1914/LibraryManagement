0550a7fbb2e8ced0e2086826437ab3e9a25817b2
krith <krith@KRITHI>
2026-02-09 13:32:49 +0530

    Initial Commit.

---
+++ LibraryManagements/src/main/java/com/kce/book/util/DBUtil.java
@@ -0,0 +1,32 @@
+package com.kce.book.util;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+    public static Connection getDBConnection() {
+        Connection connection = null;
+
+        try {
+             
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+           
+            String url = "jdbc:oracle:thin:@localhost:1521:XE";
+            String username = "KRITHIKAA";
+            String password = "system";
+
+            connection = DriverManager.getConnection(url, username, password);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Database connection error");
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
